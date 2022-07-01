fe3cb72fde2a152dd61808d55c5faf96048ac799
tepisandap <tepisandap@gmail.com>
2022-07-01 14:23:19 +0700

    Add Publish Package

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("maven-publish")
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
@@ -16,11 +17,38 @@ repositories {
 	mavenCentral()
 }
 
+apply {
+	plugin("maven-publish")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			groupId = project.properties["group"].toString()
+			artifactId = project.name
+			version = project.properties["version"].toString()
+			from(components["kotlin"])
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/Tepisandap/common")
+			credentials {
+				username = System.getenv("COMMON_USR")
+				password = System.getenv("COMMON_PSW")
+			}
+		}
+	}
+}
+
 dependencies {
 	// Spring
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.security:spring-security-core:${property("spring-security-version")}")
+	implementation("org.springframework:spring-context:${property("spring-version")}")
+	implementation("org.springframework:spring-web:5.3.20")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
